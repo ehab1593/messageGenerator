15080a92aa217bf09d61773d1daab9ad8dbbff40
Code Cademy <ehab_1593@hotmail.com>
2024-10-01 16:56:19 +0300

    Updating code and adding functionality

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
-const a = 3;
+const data = {
+    _names : ['Jack', 'David', 'Ehab', 'Hely', 'Bukayo', 'Aaron', 'Emile'],
+    _ageOfDeath : [75, 32, 69, 99, 102, 48, 78, 81],
+    _cause : ['falling from bed', 'sleeping too much', 'eating too many burgers', 'playing a lot royal match', 'flying too high'],
 
-console.log(a);
+    getNames() {
+        return this._names;
+    }, 
+
+    getRandomName() {
+       const random = Math.floor(Math.random() * this._names.length);
+       return this._names[random];
+    },
+
+    getRandomAge(){
+        const random = Math.floor(Math.random() * this._ageOfDeath.length);
+        return this._ageOfDeath[random];
+    },
+
+    getRandomCause() {
+        const random = Math.floor(Math.random() * this._cause.length);
+        return this._cause[random];
+    }
+}
+
+const randomName = data.getRandomName();
+const randomAge = data.getRandomAge();
+const randomCause = data.getRandomCause();
+
+console.log(`${randomName} will live until the age of ${randomAge} and the cause of their end will be ${randomCause}.`);
